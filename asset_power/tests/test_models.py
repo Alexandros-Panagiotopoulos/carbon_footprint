b45747f708e62b557836f9e4da82e15bd49ba337
alex <panagiotopoulos.alexandros@gmail.com>
2020-03-11 05:42:38 +0000

    add initial testing

---
+++ asset_power/tests/test_models.py
@@ -0,0 +1,21 @@
+from django.test import TestCase
+from asset_power.models import calculate_asset_power, calculate_cov_based_df, implement_cov, resample_by_half_hour
+
+
+class TestModels(TestCase):
+
+    def test_calculate_asset_power(self):
+
+        self.assertEqual(1, 1)
+
+    def test_calculate_cov_based_d(self):
+
+        self.assertEqual(1, 1)
+
+    def test_implement_cov(self):
+
+        self.assertEqual(1, 1)
+
+    def test_resample_by_half_hour(self):
+
+        self.assertEqual(1, 1)
---
+++ carbon_intensity/tests/test_views.py
@@ -0,0 +1,13 @@
+from django.test import TestCase
+from carbon_intensity.views import get_carbon_intensity, deserialize_carbon_intensity
+
+
+class TestViews(TestCase):
+
+    def test_get_carbon_intensity(self):
+
+        self.assertEqual(1, 1)
+
+    def test_deserialize_carbon_intensity(self):
+
+        self.assertEqual(1, 1)
